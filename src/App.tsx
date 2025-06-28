a66edb2537d574418390265ea6f9f82c28a7ec93
sasisekar25091996 <sasisekar25091996@gmail.com>
2025-06-28 15:24:49 +0530

    login and logout service done

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,27 @@ import './App.css';
 import './index.css';
 import CourseTable from './CourseTable';
 import ViewDialog from './app/profile/profile-view';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './components/basicComponents/components/homepage';
+import ForgetPassword from './components/basicComponents/components/forgetPassword';
+import Login from './components/basicComponents/components/login';
+import ResetPassword from './components/basicComponents/components/ressetPassword';
+import PageNotFound from './components/basicComponents/components/pageNotFound';
 function App() {
   return (
-    <div className="App">
-       <CourseTable />
-    </div>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}>
+        <Route path="" element={<CourseTable />}/>
+          {/* <Route path="configuration/*" element={<Configuration/>}/> */}
+          {/* <Route path="energy-module/*" element={<EnergyDashboardIndex/>}/> */}
+        </Route>
+        <Route path="/forget-password" element={<ForgetPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
